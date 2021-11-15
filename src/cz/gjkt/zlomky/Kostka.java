09cd7287059ed3d961416fd745cc2ff828b4dc8a
RadekNejl <83347650+RadekNejl@users.noreply.github.com>
2021-11-15 13:00:45 +0100

    Add files via upload

--- src/cz/gjkt/zlomky/Kostka.java
+++ src/cz/gjkt/zlomky/Kostka.java
@@ -15,8 +15,10 @@ public class Kostka {
         pocetSten = 6;
         random = new Random();
     }
-    public int hodCislo() {
-        return random.nextInt(pocetSten) + 1 ;
+    public static int hodCislo() {
+        Random random = new Random();
+
+        return random.nextInt(6) + 1 ;
     }
 
     public int getPocetSten() {
